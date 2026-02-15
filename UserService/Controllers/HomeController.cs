67a7f7f0a68723966d14c5c8008648a511cd8855
ghoi++ <pashas0@yandex.ru>
2026-02-15 19:12:17 +0300

    Добавьте файлы проекта.

---
+++ UserService/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
