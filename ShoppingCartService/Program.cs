7e1c40b149c9da5ea504a5119bda1d53d2abf6f4
ghoi++ <pashas0@yandex.ru>
2026-02-24 21:14:21 +0300

    init: start shopping cart service

---
+++ ShoppingCartService/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+    });
+}
+
+app.Run();
